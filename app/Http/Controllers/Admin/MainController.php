993e6cd709c58f075a4eb4229e0ccbf684f3ba14
a.vasilev <phpner@gmail.com>
2024-04-22 14:01:38 +0100

    Update admin configurations and settings

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -8,9 +8,11 @@ use App\Models\AiLaraConfig;
 use App\Settings\SettingGeneral;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\View\View;
 
 class MainController extends BaseController
 {
@@ -22,39 +24,63 @@ class MainController extends BaseController
         return view('admin.index', ['slot' => 'dd']);
     }
 
-    public function __invoke(SettingGeneral $settings){
+    public function __invoke(SettingGeneral $settings)
+    {
         return view('index', [
             'site_name' => $settings->site_name,
         ]);
     }
 
+    /**
+     * Clears the cache and saves the configuration settings.
+     *
+     * @param Request $request
+     * @param SettingGeneral $settings
+     * @return RedirectResponse|View
+     */
     public function configuration(Request $request, SettingGeneral $settings)
     {
-        //dd($settings->toArray());
-        //$settings = new SettingGeneral();
-
-/*        dd($settings->toArray());
-
-        app(SettingGeneral::class)->site_name;*/
-        //echo $settings->site_name;
-
         Artisan::call('cache:clear');
-        $aiLaraConfig = new AiLaraConfig();
 
         if (!empty($request->post())) {
-            $aiLaraConfig->save($request->post());
-            sleep(5);
+            $this->configurationSave($request, $settings);
             return redirect(route('admin.configuration'));
         }
 
-        $config = $aiLaraConfig->getAll();
+        return view('admin.configuration', ['config' => $settings->toArray()]);
 
-        return view('admin.configuration', ['config' => $config]);
+    }
+
+    /**
+     * Saves the configuration settings.
+     *
+     * @param Request $request
+     * @param SettingGeneral $settings
+     * @return SettingGeneral
+     */
+    protected function configurationSave(Request $request, SettingGeneral $settings): SettingGeneral
+    {
+        $settings->site_name = $request->post('site_name') ?? "";
+        $settings->site_active = (bool)$request->post('site_active');
+        $settings->app_name = $request->post('app_name') ?? "";
+        $settings->logo_path = $request->post('logo_path') ?? "";
+        $settings->logo_title = $request->post('logo_title') ?? "";
+        $settings->logo_height_px = $request->post('logo_height_px');
+        $settings->logo_width_px = $request->post('logo_width_px');
+        $settings->logo_width_px = $request->post('logo_width_px');
+        $settings->counter_external_code = $request->post('counter_external_code') ?? "";
+        $settings->test = $request->post('test');
+        $settings->api_key_aisearch = $request->post('api_key_aisearch') ?? "";
+        $settings->api_host = $request->post('api_host') ?? "";
+        $settings->admin_prefix = $request->post('admin_prefix') ?? "";
+
+        return $settings->save();
+
     }
 
     public function robotsTxt(Request $request)
     {
-        $robotsTxtPath = base_path().'/public/robots.txt';
+        $robotsTxtPath = base_path() . '/public/robots.txt';
 
         if (!file_exists($robotsTxtPath)) {
             $content = 'User-agent: *' . PHP_EOL;
