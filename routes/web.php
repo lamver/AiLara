cffff2f477b79440f16c16846a377f1ea85bbad9
Root <it_architect>
2024-04-22 22:15:51 +0300

    add category for blog

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,4 @@ Route::resource("/telegram-bots", AdminTelegramBotController::class)->except('sh
      ->name('task.view')
      ->where('id', '[0-9]+');*/
 
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
