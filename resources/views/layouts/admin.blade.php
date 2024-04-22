2ceddb3dc7123a088c0197d806db42d1408ca209
a.vasilev <phpner@gmail.com>
2024-04-22 10:22:59 +0100

    Add logout route and link in admin layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -160,7 +160,7 @@
             <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
             <div class="navbar-nav">
                 <div class="nav-item text-nowrap">
-                    <a class="nav-link px-3" href="#">Sign out</a>
+                    <a class="nav-link px-3" href="{{route('logout')}}">Sign out</a>
                 </div>
             </div>
         </header>
--- routes/auth.php
+++ routes/auth.php
@@ -56,4 +56,6 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
+    Route::get('logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->name('logout');
 });
