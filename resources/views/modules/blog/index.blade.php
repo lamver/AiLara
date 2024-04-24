4bc02c7a1029d6441f9a4d3d5021ed7a4415a84a
Root <it_architect>
2024-04-24 14:43:25 +0300

    add sitemap posts

--- resources/views/modules/blog/index.blade.php
+++ resources/views/modules/blog/index.blade.php
@@ -112,9 +112,6 @@
                                     <a href="{{ $post->urlToPost }}" title="{{ $post->seo_title }}" class="stretched-link btn btn-link">Читать</a>
                                     <p class="card-text">
                                         <small class="text-muted">
-                                            <svg title="{{ $post->updated_at }}" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
-                                                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
-                                            </svg>&nbsp;
                                             {{ \Illuminate\Support\Carbon::create($post->updated_at)->shortRelativeDiffForHumans(date("Y-m-d h:i:s", time())) }}
                                             &nbsp;
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-dots-fill" viewBox="0 0 16 16">
