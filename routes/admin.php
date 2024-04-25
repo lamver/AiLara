55b70ea596c9486e8a93645302c8f6d378909c5c
a.vasilev <phpner@gmail.com>
2024-04-25 20:19:07 +0100

    Improve indentation in routes file

--- routes/admin.php
+++ routes/admin.php
@@ -21,38 +21,37 @@ use App\Services\Translation\Translation;
 |
 */
 
-
 Route::middleware(['auth', 'verified'])->prefix(Translation::checkRoutePrefix())->group(function () {
-Route::prefix(Config::get('ailara.admin_prefix'))->group(function (){
-    Route::get('/', [MainController::class, 'index'])->name('admin.index');
-    Route::get('/configuration', [MainController::class, 'configuration'])->name('admin.configuration');
-    Route::post('/configuration', [MainController::class, 'configuration'])->name('admin.configuration.save');
-    Route::get('/configuration/robots_txt', [MainController::class, 'robotsTxt'])->name('admin.configuration.robots_txt');
-    Route::post('/configuration/robots_txt', [MainController::class, 'robotsTxt'])->name('admin.configuration.robots_txt.save');
+    Route::prefix(Config::get('ailara.admin_prefix'))->group(function () {
+        Route::get('/', [MainController::class, 'index'])->name('admin.index');
+        Route::get('/configuration', [MainController::class, 'configuration'])->name('admin.configuration');
+        Route::post('/configuration', [MainController::class, 'configuration'])->name('admin.configuration.save');
+        Route::get('/configuration/robots_txt', [MainController::class, 'robotsTxt'])->name('admin.configuration.robots_txt');
+        Route::post('/configuration/robots_txt', [MainController::class, 'robotsTxt'])->name('admin.configuration.robots_txt.save');
 
-    Route::get('/ais/common-data', [AiSearchController::class, 'commonData'])->name('admin.ais.commonData');
+        Route::get('/ais/common-data', [AiSearchController::class, 'commonData'])->name('admin.ais.commonData');
 
-    Route::get('/pages', [SeoPages::class, 'seoPagesList'])->name('admin.ais.pages');
-    Route::get('/page-edit/{id}', [SeoPages::class, 'seoPageEdit'])->name('admin.ais.page.edit');
-    Route::post('/page-save/{id}', [SeoPages::class, 'seoPageSave'])->name('admin.ais.page.save');
+        Route::get('/pages', [SeoPages::class, 'seoPagesList'])->name('admin.ais.pages');
+        Route::get('/page-edit/{id}', [SeoPages::class, 'seoPageEdit'])->name('admin.ais.page.edit');
+        Route::post('/page-save/{id}', [SeoPages::class, 'seoPageSave'])->name('admin.ais.page.save');
 
-    Route::get('/ais/ai-forms', [AiSearchController::class, 'aiForms'])
-        ->name('admin.ais.aiForms');
-    Route::get('/ais/ai-forms/new-form', [AiSearchController::class, 'newForm'])
-        ->name('admin.ais.aiForms.newForm');
-    Route::post('/ais/ai-forms/new-form-create', [AiSearchController::class, 'newFormCreate'])
-        ->name('admin.ais.aiForms.newFormCreate');
-    Route::any('/ais/ai-forms/form-edit/{formId}', [AiSearchController::class, 'formEdit'])
-        ->name('admin.ais.aiForms.formEdit')
-        ->where('formId', '[0-9]+');
-    Route::get('/ais/ai-forms/form-delete/{formId}', [AiSearchController::class, 'formDelete'])
-        ->name('admin.ais.aiForms.formDelete')
-        ->where('formId', '[0-9]+');
-    Route::get('/update', [\App\Http\Controllers\Admin\UpdateController::class, 'index'])->name('admin.update');
-    Route::get('/logs', [\App\Http\Controllers\Admin\LogsController::class, 'index'])->name('admin.logs');
-    Route::get('/setLang/{locale}', function (string $locale) {
-        app()->setLocale($locale);
-        session()->put('locale', $locale);
-    })->name('setLang');
-});
+        Route::get('/ais/ai-forms', [AiSearchController::class, 'aiForms'])
+            ->name('admin.ais.aiForms');
+        Route::get('/ais/ai-forms/new-form', [AiSearchController::class, 'newForm'])
+            ->name('admin.ais.aiForms.newForm');
+        Route::post('/ais/ai-forms/new-form-create', [AiSearchController::class, 'newFormCreate'])
+            ->name('admin.ais.aiForms.newFormCreate');
+        Route::any('/ais/ai-forms/form-edit/{formId}', [AiSearchController::class, 'formEdit'])
+            ->name('admin.ais.aiForms.formEdit')
+            ->where('formId', '[0-9]+');
+        Route::get('/ais/ai-forms/form-delete/{formId}', [AiSearchController::class, 'formDelete'])
+            ->name('admin.ais.aiForms.formDelete')
+            ->where('formId', '[0-9]+');
+        Route::get('/update', [\App\Http\Controllers\Admin\UpdateController::class, 'index'])->name('admin.update');
+        Route::get('/logs', [\App\Http\Controllers\Admin\LogsController::class, 'index'])->name('admin.logs');
+        Route::get('/setLang/{locale}', function (string $locale) {
+            app()->setLocale($locale);
+            session()->put('locale', $locale);
+        })->name('setLang');
+    });
 });
