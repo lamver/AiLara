99cc77fc75d7b080f45168e5e65ed7bc4df134d9
valeriifrolov@gmail.com <1Q@w3easdzxc>
2024-05-01 23:22:04 +0200

    fix module blog category save

--- app/Models/Modules/Blog/Category.php
+++ app/Models/Modules/Blog/Category.php
@@ -52,6 +52,10 @@ class Category extends Model
             $model->slug = Str::slug($model->title);
         }
 
+        if ($model->parent_id == 0) {
+            $model->parent_id = null;
+        }
+
         try {
             return $model->save();
         } catch (\Exception $exception) {
